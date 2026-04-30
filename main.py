d060db1f061c457785249844cc87284332f2b329
almaradevice <almara.de.vice@gmail.com>
2026-04-30 23:08:21 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os, subprocess
 
-subprocess.Popen([
+print(">>>", subprocess.Popen([
     'python', 'web-app.py'
-], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, start_new_session=True)
+], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, start_new_session=True))
 
 os.system('python bot.py')
